5bd331741d848687c4c03853c6153927fb98df7d
Luangoma <101829176+Luangoma@users.noreply.github.com>
2023-10-23 11:24:35 +0200

    AStar implementation
    Comienzo de la implementación de AStar.
    Se debe continuar con la creación de la clase nodo y el algoritmo AStar.
    La clase Node debe incluir:
    - CellInfo
    - NodoPadre
    - Coste (de ida)
    - Metodos
    etc

--- InteligenciaArtificial/Assets/Scripts/GrupoA/AStar/AStar.cs
+++ InteligenciaArtificial/Assets/Scripts/GrupoA/AStar/AStar.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Navigation.Interfaces;
 using Navigation.World;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 namespace Navigation.Algorithms.AStar
@@ -20,7 +22,8 @@ namespace Navigation.Algorithms.AStar
         private WorldInfo _world;
         private Directions _currentDirection = Directions.None;
         private int stepCount = 0;
-
+        private List<Node> openList;
+        private Node[] sucesors;
 
         public void Initialize(WorldInfo worldInfo, INavigationAlgorithm.AllowedMovements allowedMovements)
         {
@@ -30,6 +33,40 @@ namespace Navigation.Algorithms.AStar
         public CellInfo[] GetPath(CellInfo startNode, CellInfo targetNode)
         {
             throw new NotImplementedException();
+        }
+
+        public Node SpaceStateSearch(Node initialState)
+        {
+            Node current = new Node();
+            openList.Add(initialState);
+            do
+            {
+                current = openList.First();
+                if (current.GetCellInfo().Type = CellInfo.CellType.Exit)
+                {
+                    return current;
+                }
+                else
+                {
+                    sucesors = expand(current);
+                    foreach (CellInfo sucesor in sucesors)
+                    {
+                        openList.Add(sucesor);
+                    }
+                }
+            } while ((openList != null));
+            return null;
+        }
+        public Node[] expand(Node nodeToExpand)
+        {
+            Node[] nodes = null;
+            int i = 0;
+            foreach (Node newNode in nodeToExpand)
+            {
+                nodes[i] = newNode;
+                i++;
+            }
+            return nodes;
         }
     }
 }
---
+++ InteligenciaArtificial/Assets/Scripts/GrupoA/AStar/Node.cs
@@ -0,0 +1,21 @@
+﻿using Navigation.World;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.GrupoA.AStar
+{
+    internal class Node
+    {
+        public CellInfo celdaInfo;
+
+        public Node() {
+            
+        }
+
+        public CellInfo GetCellInfo(CellInfo celdaInfo) 
+        { return celdaInfo; }
+    }
+}
